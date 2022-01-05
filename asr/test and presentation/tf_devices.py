3960dc616591757c7db3ff1e0fa8c6e3b402ca79
Eric Chandler <echandler@uchicago.edu>
2022-01-05 14:44:29 -0600

    log name of compute node

--- asr/test and presentation/tf_devices.py
+++ asr/test and presentation/tf_devices.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import subprocess
 import tensorflow as tf
 
 logger = logging.getLogger('env')
@@ -7,6 +8,8 @@ logger = logging.getLogger('env')
 num_cpu = len(tf.config.list_physical_devices('CPU'))
 num_gpu = len(tf.config.list_physical_devices('GPU'))
 cuda_devs = os.environ['CUDA_VISIBLE_DEVICES']
+hostname = subprocess.run("hostname").stdout
+logger.info(f"Running on host: {hostname}")
 logger.info(f"Num CPUs Available: {num_cpu}")
 logger.info(f"Num GPUs Available: {num_gpu}")
 logger.info(f"CUDA visible devices: {cuda_devs}")
