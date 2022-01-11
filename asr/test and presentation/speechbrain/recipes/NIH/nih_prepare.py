aa0047960e1684ac21e6b657e8422954aad44047
Eric Chandler <echandler@uchicago.edu>
2022-01-10 22:53:48 -0600

    configure tokenizer for training

--- asr/test and presentation/speechbrain/recipes/NIH/nih_prepare.py
+++ asr/test and presentation/speechbrain/recipes/NIH/nih_prepare.py
@@ -6,25 +6,44 @@ import pandas as pd
 
 logger = logging.getLogger(__name__)
 
-def prepare_nih(data_folder, save_folder, skip_prep=False):
+def prepare_nih(cluster: str, 
+                dataset_name:str, 
+                num_train: int, 
+                split_ratios: dict,
+                save_folder: str, 
+                skip_prep=False):
     """
     @:param
-        data_folder : path to dataset.
+        cluster: cluster name. either ['rcc', 'ai']
+        dataset_name: either ['radio', 'librispeech', 'atczero']
+        num_train: number of samples to prepare from dataset
+        split_ratios: dictionary of named train/val/test splits and fraction of dataset
         save_folder: path to manifest (output) folder
         skip_prep: If True, skip data preparation.
     """
     if skip_prep:
         return
 
-    # TODO: pass in constructor arguments somehow
-    data = RadioDataset('rcc', nrow=1024).data
+    if dataset_name == 'radio':
+        data = RadioDataset(cluster, nrow=num_train).data
+    elif dataset_name == 'librispeech':
+        data = LibriSpeechDataset(cluster, nrow=num_train).data
+    else:
+        raise NotImplementedError('dataset ' + dataset_name)
     
-    # Split into train/val/test
-    test_data = data.sample(frac=.2, random_state=1234)
-    trainval_data = data.iloc[data.index.difference(test_data.index)]
-    val_data = trainval_data.sample(frac=.25, random_state=1234)
-    train_data = trainval_data.iloc[trainval_data.index.difference(val_data.index)]
-    splits = {'train': train_data, 'val': val_data, 'test': test_data}
+    # Split into hparams-defined splits
+    splits = {}
+    other_splits = data
+    for split, frac in split_ratios.items():
+        current_split = other_splits.sample(frac=frac, random_state=1234)
+        other_splits = other_splits.iloc[other_splits.index.difference(current_split.index)]
+        splits[split] = current_split
+        
+    #test_data = data.sample(frac=.2, random_state=1234)
+    #trainval_data = data.iloc[data.index.difference(test_data.index)]
+    #val_data = trainval_data.sample(frac=.25, random_state=1234)
+    #train_data = trainval_data.iloc[trainval_data.index.difference(val_data.index)]
+    #splits = {'train': train_data, 'val': val_data, 'test': test_data}
     
     for split, splitdata in splits.items():
         manifest_path = os.path.join(save_folder, split) + '.csv'
