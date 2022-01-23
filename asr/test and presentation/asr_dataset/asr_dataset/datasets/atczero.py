dd26e80973f96e8b0ba631a0b87d4270041fa8dc
Eric Chandler <echandler@uchicago.edu>
2022-01-23 10:58:24 -0600

    skeleton code

--- asr/test and presentation/asr_dataset/asr_dataset/datasets/atczero.py
+++ asr/test and presentation/asr_dataset/asr_dataset/datasets/atczero.py
@@ -1,23 +1,34 @@
 '''
 File: atczero.py
 Brief: Loader for ATC0 corpus.
-Authors: Eric Chandler <echandler@uchicago.edu>
+Authors: William Dolan <wdolan@uchicago.edu>
 '''
 
-import os
-import re
 import datetime
 import pandas as pd
-from dataset import AudioClipDataset
+import logging
+from asr_dataset.dataset import AudioClipDataset
+from asr_dataset.datasets.constants import DATASET_DIRS
 
 class ATCZeroDataset(AudioClipDataset):
 
-    @classmethod
-    def load_transcripts(cls, transcripts_dir, sample_rate=SAMPLE_RATE):
+    def __init__(self, cluster:str='rcc', nrow: int=None, frac: float=None, window_len=WINDOW_LEN):
+        """
+        Returns a ATCZeroDataset with a data attribute which is a dataframe of:
+            path to utterance audio, duration (sec), number of samples, transcript text, [other columns]
+        """
+        super().__init__('ATC0', nrow, frac, window_len)
+        raise NotImplementedError('TODO')
+
+    def _load_transcripts(self, sample_rate=SAMPLE_RATE):
         """
         This function is to get audios and transcripts needed for training
-        Params:
-            @transcripts_dir: path to directory with transcripts csvs
+        Returns: a dataframe with columns: path to utterance audio, transcripts
         """
-        pass
+        # XXX: This function is called by the base class's init method. It's kind of a weird 
+        #       way to implement subclassing & maybe should be refactored. But for for now
+        #       this function should do most of the class-specific data loading work.
+        # XXX: This funciton is kind of weird. It is actually called by the base class's init method.
+        #       So this function should do most of the work of
+        raise NotImplementedError('todo')
  
