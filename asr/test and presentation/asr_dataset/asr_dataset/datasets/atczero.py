288611379cf132cd7335e6bd5c8c4578859a8ac5
William Patrick Dolan <wdolan@midway3-login2.rcc.local>
2022-01-24 23:17:33 -0600

    Progress on atc0 dataset loader

--- asr/test and presentation/asr_dataset/asr_dataset/datasets/atczero.py
+++ asr/test and presentation/asr_dataset/asr_dataset/datasets/atczero.py
@@ -4,6 +4,9 @@ Brief: Loader for ATC0 corpus.
 Authors: William Dolan <wdolan@uchicago.edu>
 '''
 
+WINDOW_LEN = .02 #Sec
+SAMPLE_RATE = 8000
+
 import datetime
 import pandas as pd
 import logging
@@ -17,10 +20,13 @@ class ATCZeroDataset(AudioClipDataset):
         Returns a ATCZeroDataset with a data attribute which is a dataframe of:
             path to utterance audio, duration (sec), number of samples, transcript text, [other columns]
         """
+        self.transcripts_dir = DATASET_DIRS[cluster]['atc0']
+        
         super().__init__('ATC0', nrow, frac, window_len)
-        raise NotImplementedError('TODO')
+        self.data = self.add_duration(self.data)
+        self.data = self.add_sample_count(self.data)
 
-    def _load_transcripts(self, sample_rate=SAMPLE_RATE):
+    def _load_transcripts(self, sample_rate=SAMPLE_RATE, window_len = WINDOW_LEN):
         """
         This function is to get audios and transcripts needed for training
         Returns: a dataframe with columns: path to utterance audio, transcripts
@@ -30,5 +36,9 @@ class ATCZeroDataset(AudioClipDataset):
         #       this function should do most of the class-specific data loading work.
         # XXX: This funciton is kind of weird. It is actually called by the base class's init method.
         #       So this function should do most of the work of
-        raise NotImplementedError('todo')
+
+        unloaded_df = pd.read_csv(self.transcripts_dir + '/atc0.csv')
+
+        loaded_df = unloaded_df
+        loaded_df['offset'] = loaded_df['start']
  
