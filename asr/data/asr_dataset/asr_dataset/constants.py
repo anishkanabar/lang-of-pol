0aa1817ea40ba6ad4fb5e3e56f9f55cad4754b05
Eric Chandler <echandler@uchicago.edu>
2022-01-31 22:16:44 -0600

    clean up after refactor

--- asr/data/asr_dataset/asr_dataset/constants.py
+++ asr/data/asr_dataset/asr_dataset/constants.py
@@ -3,12 +3,12 @@ DATASET_DIRS = {
         'police_transcripts':'/project/graziul/transcripts',
         'police_mp3s':'/project/graziul/data',
         'librispeech':'/project/graziul/ra/shiyanglai/experiment1/audio-data/LibriSpeech/train-clean-100',
-        'atczero' : '/project/graziul/data/corpora/atc0_comp',
+        'atczero' : '/project/graziul/ra/wdolan/atc0_comp',
     },
     'ai': {
         'police_transcripts':'/net/projects/uri/transcripts',
         'police_mp3s':'/net/projects/uri/data',
         'librispeech':'/net/projects/uri/ra/shiyanglai/experiment1/audio data/LibriSpeech/train-clean-100',
-        'atczero': '/net/projects/uri/data/corpora/atc0_comp',
+        'atczero': '/net/projects/uri/ra/wdolan/atc0_comp',
     }
 }
