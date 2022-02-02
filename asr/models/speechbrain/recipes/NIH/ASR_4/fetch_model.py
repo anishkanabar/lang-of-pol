278adef06e9ba370c099b2f8105cf008609db45f
Eric Chandler <echandler@uchicago.edu>
2022-02-01 22:42:35 -0600

    try ctc model

---
+++ asr/models/speechbrain/recipes/NIH/ASR_4/fetch_model.py
@@ -0,0 +1,11 @@
+from speechbrain.lobes.models.huggingface_wav2vec import HuggingFaceWav2Vec2
+
+model_name = "facebook/wav2vec2-base-100h"
+revision = "main"
+save_path = "/project/graziul/ra/echandler/scratch/" + model_name
+output_norm = True
+freeze = True
+#HuggingFaceWav2Vec2(model_name, save_path, output_norm, freeze)
+
+from huggingface_hub import hf_hub_download, snapshot_download
+snapshot_download(model_name, revision, save_path)
