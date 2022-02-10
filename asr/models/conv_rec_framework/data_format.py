464cee7331fd2748c687f38503a5108c7fe41c83
shiyanglai <shiyanglai@uchicago.edu>
2022-02-10 00:05:41 -0600

    update pyannote data loader

--- asr/models/conv_rec_framework/data_format.py
+++ asr/models/conv_rec_framework/data_format.py
@@ -5,6 +5,7 @@ email: shiyanglai@uchicago.edu
 """
 
 from pyannote.database import get_protocol, FileFinder
+import os
 
 
 def load_data(corpus='ami'):
@@ -15,6 +16,32 @@ def load_data(corpus='ami'):
     preprocessors = {'audio': FileFinder()}
     protocol = get_protocol(f"{corpus.upper()}.SpeakerDiarization.only_words", preprocessors=preprocessors)
     return protocol
+
+
+def make_corpus_strcuture(root_path, corpus_name):
+    """
+    construct the folders' structure of the corpus
+    """
+    if not os.path.exists(root_path + corpus_name):
+        os.makedirs(root_path + corpus_name)
+        # create three subfolders: lists, rttms, uems
+        if not os.path.exists(root_path + corpus_name + '/lists'):
+            os.makedirs(root_path + corpus_name + '/lists')
+        else:
+            print(f"Directory '{root_path + corpus_name + '/lists'}' already exist. Please delete it and try again.")
+        
+        if not os.path.exists(root_path + corpus_name + '/rttms'):
+            os.makedirs(root_path + corpus_name + '/rttms')
+        else:
+            print(f"Directory '{root_path + corpus_name + '/rttms'}' already exist. Please delete it and try again.")
+
+        if not os.path.exists(root_path + corpus_name + '/uems'):
+            os.makedirs(root_path + corpus_name + '/uems')
+        else:
+            print(f"Directory '{root_path + corpus_name + '/uems'}' already exist. Please delete it and try again.")
+        print('The structure of corpus has been set successfully.')
+    else:
+        print(f"Directory '{root_path + corpus_name}' already exist. Please delete it and try again.")
 
 
 def generate_rttm():
