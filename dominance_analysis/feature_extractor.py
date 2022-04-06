f4b016ea4063191aaf5abefe8174a9de1197d4cb
Ajay-26 <aj.sailopal@gmail.com>
2022-04-06 14:00:58 -0500

    OMG IT WORKS GOD IS REAL THANANK YOU

--- dominance_analysis/feature_extractor.py
+++ dominance_analysis/feature_extractor.py
@@ -4,11 +4,10 @@ import time
 import tracemalloc
 import argparse
 
-def make_config_string(lld_list,func_list):
+def make_config_string(feature_list, lld_list,func_list):
     lld_str = ''
     for lld in lld_list:
         lld_str = lld_str + '{_lld} = 1\n'.format(_lld = lld)
-    print(lld_str)
     
     func_dict = {'variance':0,'stddev':0,'amean':0,'skewness':0,'kurtosis':0,'doRatioLimit':0}
     for key in list(func_dict.keys()):
@@ -18,10 +17,9 @@ def make_config_string(lld_list,func_list):
     func_str = ''
     for key in list(func_dict.keys()):
         func_str = func_str + 'Moments.{_key} = {_val}\n'.format(_key = key, _val = func_dict[key])
-    print(func_str)
     
-
-    str1 = '''
+    
+    config_str = '''
     [componentInstances:cComponentManager]
     instance[dataMemory].type=cDataMemory
 
@@ -37,9 +35,15 @@ def make_config_string(lld_list,func_list):
 
     [componentInstances:cComponentManager]
     instance[framer].type = cFramer
-    instance[lld].type = cEnergy
-    instance[func].type=cFunctionals
-
+    '''
+    
+    inst_str = ''
+    for feature in feature_list:
+        inst_str = inst_str + 'instance[{_feat}].type = c{_feat}\n'.format(_feat = feature)
+    config_str = config_str + inst_str
+    
+    config_str = config_str + '''instance[func].type=cFunctionals
+    
     [framer:cFramer]
     reader.dmLevel = wave
     writer.dmLevel = frames
@@ -50,33 +54,44 @@ def make_config_string(lld_list,func_list):
     frameCenterSpecial = left
     noPostEOIprocessing = 1
 
-    [lld:cEnergy]
-    reader.dmLevel = frames
-    writer.dmLevel = lld
-    \{\cm[bufferModeRbConf{?}:path to included config to set the buffer mode for the standard ringbuffer levels]}
-    nameAppend = energy
-    copyInputName = 1
     '''
-
-    str2 = '''[func:cFunctionals]
-    reader.dmLevel=lld
+    
+    desc_str = ''
+    feat_str = ''
+    for feature in feature_list:
+        feat_str = feat_str + feature + ';'
+        desc_str = desc_str + '''[{_feat}:c{_feat}]
+        reader.dmLevel = frames
+        writer.dmLevel = {_feat}
+        '''.format(_feat = feature) + '\{\cm[bufferModeRbConf{?}:path to included config to set the buffer mode for the standard ringbuffer levels]}' + '''
+        nameAppend = {_feat}
+        copyInputName = 1
+        '''.format(_feat=feature) + '\n'
+    config_str = config_str + desc_str
+    feat_str = feat_str[:-1]
+    
+    
+    func_append = '''    [func:cFunctionals]
+    reader.dmLevel=%s
     writer.dmLevel=func
     copyInputName = 1
     \{\cm[bufferModeRbConf]}
     \{\cm[frameModeFunctionalsConf{?}:path to included config to set frame mode for all functionals]}
     functionalsEnabled=Moments
-    '''
-
-    str3 = '''
+    ''' % feat_str
+    
+    config_str = config_str + func_append + func_str
+    end_str = '''
 
     ;;; sink
 
     \{\cm[sink{?}:include external sink]}
 
     '''
-    config_str = str1 + lld_str + str2 + func_str + str3
+    config_str = config_str + end_str
+    print(config_str)
     return config_str
-
+    
 if __name__ == 'main':
     pkl_path = '/project/graziul/ra/ajays/whitelisted_vad_dict.pkl'
     file = open(pkl_path,'rb')
@@ -85,12 +100,14 @@ if __name__ == 'main':
 
 
     parser = argparse.ArgumentParser(description='Process Args')
+    parser.add_argument("--features", nargs="+", default=["Energy"])
     parser.add_argument("--lld", nargs="+", default=["rms", "log"])
     parser.add_argument("--functionals", nargs="+", default=["stddev","amean"])
 
     args = parser.parse_args()
 
+    features_list = args.features
     lld_list = args.lld
     func_list = args.functionals
-    print(lld_list, func_list)
-    make_config_string(lld_list,func_list)
+    print(features_list,lld_list, func_list)
+    make_config_string(features_list,lld_list,func_list)
