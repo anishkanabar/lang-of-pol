656200a8c24ba93f485289e6b01b68287d2fc467
Ajay-26 <aj.sailopal@gmail.com>
2022-04-07 13:42:58 -0500

    feature extractor code

--- dominance_analysis/feature_extractor.py
+++ dominance_analysis/feature_extractor.py
@@ -4,21 +4,23 @@ import time
 import tracemalloc
 import argparse
 
-def make_config_string(feature_list, lld_list,func_list):
-    lld_str = ''
-    for lld in lld_list:
-        lld_str = lld_str + '{_lld} = 1\n'.format(_lld = lld)
-    
+def available_features():      #Displays the features which can be obtained
+    features = ['Energy','Melspec','Intensity','Mfcc']
+    return features
+
+def make_config_string(feature_list,func_list):    #Makes a config file using the feature list and func list
     func_dict = {'variance':0,'stddev':0,'amean':0,'skewness':0,'kurtosis':0,'doRatioLimit':0}
+    #Preparing dictionaries to be used in the future
     for key in list(func_dict.keys()):
         if key in func_list:
             func_dict[key] = 1
             
+    #Preparing strings to be used
     func_str = ''
     for key in list(func_dict.keys()):
         func_str = func_str + 'Moments.{_key} = {_val}\n'.format(_key = key, _val = func_dict[key])
     
-    
+    #This part is necessary regardless of input
     config_str = '''
     [componentInstances:cComponentManager]
     instance[dataMemory].type=cDataMemory
@@ -37,6 +39,7 @@ def make_config_string(feature_list, lld_list,func_list):
     instance[framer].type = cFramer
     '''
     
+    #instantiating the feature sections
     inst_str = ''
     for feature in feature_list:
         inst_str = inst_str + 'instance[{_feat}].type = c{_feat}\n'.format(_feat = feature)
@@ -56,6 +59,7 @@ def make_config_string(feature_list, lld_list,func_list):
 
     '''
     
+    #Coding descriptions for each of the features in the input. The further arguments would need to be provided here
     desc_str = ''
     feat_str = ''
     for feature in feature_list:
@@ -70,7 +74,7 @@ def make_config_string(feature_list, lld_list,func_list):
     config_str = config_str + desc_str
     feat_str = feat_str[:-1]
     
-    
+    #Functionals to be computed for each of the features. This function is complete
     func_append = '''    [func:cFunctionals]
     reader.dmLevel=%s
     writer.dmLevel=func
@@ -92,7 +96,7 @@ def make_config_string(feature_list, lld_list,func_list):
     print(config_str)
     return config_str
     
-if __name__ == 'main':
+if __name__ == 'main': #Will be called on running >python feature_extractor.py
     pkl_path = '/project/graziul/ra/ajays/whitelisted_vad_dict.pkl'
     file = open(pkl_path,'rb')
     vad_dict = pickle.load(file)
@@ -101,13 +105,11 @@ if __name__ == 'main':
 
     parser = argparse.ArgumentParser(description='Process Args')
     parser.add_argument("--features", nargs="+", default=["Energy"])
-    parser.add_argument("--lld", nargs="+", default=["rms", "log"])
     parser.add_argument("--functionals", nargs="+", default=["stddev","amean"])
 
     args = parser.parse_args()
 
     features_list = args.features
-    lld_list = args.lld
     func_list = args.functionals
-    print(features_list,lld_list, func_list)
-    make_config_string(features_list,lld_list,func_list)
+    print(features_list, func_list)
+    make_config_string(features_list,func_list)
