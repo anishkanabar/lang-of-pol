52840af994fe564006c09e40c8ee57b5f19c5cbd
Eric Chandler <echandler@uchicago.edu>
2022-05-01 11:23:12 -0500

    re-run after fixing input output ratio and without filtering minimum length
    - trial 42: after putting ratio filter after character filter
    - trial 43: kept failing for environmental bs
    - trial 44: after removing miminum audio length filter

--- asr/models/speechbrain/ctc/ctc_prepare.py
+++ asr/models/speechbrain/ctc/ctc_prepare.py
@@ -2,6 +2,8 @@ import re
 import os
 import logging
 import bpc_prepare as prepare
+from asr_dataset.base import AsrETL
+from asr_dataset.atczero import ATCZeroETL
 import pandas as pd
 
 logger = logging.getLogger('asr.prepare.ctc')
@@ -24,40 +26,15 @@ def prepare_bpc(split_ratios: dict,
 
     splits = get_splits(split_ratios, output_folder)
     splits = {k: ctc_prep(v) for k, v in splits.items()}
-    splits = {k: filter_duration(v) for k, v in splits.items()}
-    # Commentint this causes error!
-    splits = {k: filter_ratio(v) for k, v in splits.items()}
-    # Commentint this causes error!
+    # splits = {k: filter_duration(v) for k, v in splits.items()}
     splits = {k: filter_nonalphanum(v) for k,v in splits.items()}
+    splits = {k: filter_ratio(v) for k, v in splits.items()}
 
-    # blacklist = get_blacklist(blacklist_file)
-
-    # Commenting out first pass because it was written assuming it
-    # could test all data points independently. This is infeasible
-    # due to high computational cost per data point (each incurs the
-    # startup costs of re-loading the model from scratch).
-    # Instead, we'll run the first pass enough times to be confident
-    # we didn't miss any individually problematic data points.
-    # ...
-    # Ran first pass and got 474 negative results. Previous blacklist
-    # had ~ 450 positive results out of 30k training points
-    # = probability of positive result = .015
-    # => prob of randomly sampling 474 negative results
-    # = (1 - prob of positive data point)^m 
-    # = (1-.015)^474 = .00077 => very low probability of Type II error
-    # ...
-    # splits = {k: first_pass(v, blacklist) for k, v in splits.items()}
-    # splits = {k: second_pass(v, blacklist) for k, v in splits.items()}
+    for k, v in splits.items():
+        logger.info(f'{k} dataset stats:')
+        AsrETL._describe(v)
 
     write_splits(splits, output_folder)
-
-
-def get_blacklist(blacklist_file: str):
-    if not os.path.exists(blacklist_file):
-        with open(blacklist_file, "w") as f:
-            f.write("seed,pass,batch,ID,is_finite,loss,wrd\n")
-
-    return pd.read_csv(blacklist_file)
 
 
 def get_splits(split_ratios, output_folder) -> {str, pd.DataFrame}:
@@ -85,6 +62,7 @@ def filter_duration(df: pd.DataFrame) -> pd.DataFrame:
     df = df.loc[predicate]
     return df
 
+
 def filter_nonalphanum(df: pd.DataFrame) -> pd.DataFrame:
     # regex gotchas: must escape [], -, / even if inside brackets
     special = re.compile("[^A-Za-z0-9 ']")
@@ -100,43 +78,15 @@ def filter_ratio(df: pd.DataFrame) -> pd.DataFrame:
     Params:
         df - expects columns {duration, wrd} 
     """
-    HOP_DURATION = 20  # (ms)
     FRAME_RATE = 49  # (Hz)
-    MIN_RATIO = 2.84375 # (2.0 is bad --- 2.75 was bad -- 2.796875 bad -- 2.84375 ok -- 2.9375 ok -- 3.125 ok -- 3.5 is ok -- 5.0 is ok)
+    #MIN_RATIO = 2.84375 # (2.0 is bad --- 2.75 was bad -- 2.796875 bad -- 2.84375 ok -- 2.9375 ok -- 3.125 ok -- 3.5 is ok -- 5.0 is ok)
+    MIN_RATIO = 1.0
     logger.info(f'Testing with MFCC ratio {MIN_RATIO}')
-    hop_sec = HOP_DURATION / 1000
     mfcc_lengths = df['duration'] * FRAME_RATE
     # logger.debug(f'Min/Avg/Max Num Frames: {mfcc_lengths.min():.2f} : {mfcc_lengths.mean():.2f} : {mfcc_lengths.max():.2f}')
     num_chars = df['wrd'].str.len()
     mfcc_ratios = mfcc_lengths / num_chars
     pred = mfcc_ratios > MIN_RATIO
     logger.info(f"Discarding {len(pred) - pred.sum()} bad MFCC ratios of {len(pred)} examples.")
-    # for i in range(len(pred)):
-    #     if pred[i]:
-    #         logger.debug(f"GOOD, ID, {df.loc[i,'ID']}, NFRAMES, {mfcc_lengths[i]:.2f}, NCHARS, {num_chars[i]}, TXT, {df.loc[i, 'wrd']}")
-    #     else:
-    #         logger.debug(f"BAD, ID, {df.loc[i,'ID']}, NFRAMES, {mfcc_lengths[i]:.2f}, NCHARS, {num_chars[i]}, TXT, {df.loc[i, 'wrd']}")
     return df.loc[pred]
-
-
-
-def first_pass(df: pd.DataFrame, blacklist: pd.DataFrame) -> pd.DataFrame:
-    """First pass: Train on a single utterance to identify problems independent of sequencing."""
-    singles = blacklist.loc[(blacklist['pass'] == 'first') & (blacklist['batch'] == 0), 'ID']
-    predicate = ~ df['ID'].isin(singles)
-    if predicate.any():
-        df = df.loc[predicate].head(1)
-        logger.info("Blacklist first pass data point {}: {}".format(df['ID'].values[0], df['wrd'].values[0]))
-    else:
-        raise RuntimeError("First pass is done!")
-    return df
-        
-        
-def second_pass(df: pd.DataFrame, blacklist: pd.DataFrame) -> pd.DataFrame:
-    """Second pass: Train on all non-blacklisted utterances. Grow blacklist one at a time."""
-    infinite_ids = blacklist.loc[~(blacklist['is_finite'].astype(bool)), 'ID']
-    predicate = df['ID'].isin(infinite_ids)
-    logger.info("Blacklist second pass: filtering out {} audio".format(predicate.sum()))
-    df = df.loc[~predicate]
-    return df
 
